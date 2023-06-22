93b39ed069aad426f0c66bfc0c7ed94da650c7ca
Humberto Nacif <gigantedeluz@gmail.com>
2023-06-22 11:07:15 -0300

    assigning functions to buttons

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -5,18 +5,20 @@ import styles from './Form.module.css';
 const Form = () => {
     const [height, setHeight] = useState(0);
     const [weight, setWeight] = useState(0);
+    const [showBMI, setShowBMI] = useState(false);
 
-    const renderBMI = () => {
+    const calculateBMI = () => {
         let squareHeight = height * height;
         let valueBMI = weight / squareHeight;
 
-        if (height && weight > 0) {
+        if (height > 0 && weight > 0) {
+            setShowBMI(true);
             return valueBMI;
         }
     }
 
     return (
-        <form>
+        <form onSubmit={calculateBMI}>
             <div className={styles.container_form}>
                 <div className="row align-items-center mt-1 mb-3">
                     <div className="col-md-2">
@@ -31,12 +33,25 @@ const Form = () => {
                     <div className="w-100 mt-1"></div>
                     <div className="col-md-2 d-flex">
                         <button id="calcBtn" className="btn btn-primary ps-3 pe-4 rounded-pill me-4" type="submit">Calculate</button>
-                        <button className="btn btn-warning ps-4 pe-5 pt-2 pb-2 rounded-pill" type="reset">Clear</button>
+                        <button 
+                            className="btn btn-warning ps-4 pe-5 pt-2 pb-2 rounded-pill" 
+                            type="reset"
+                            onClick={() => {
+                                setHeight(0);
+                                setWeight(0);
+                                setShowBMI(false);
+                            }}>
+                            Clear
+                        </button>
                     </div>
                 </div>
                 <div className="row align-items-center">
                     <div className="col-md-6">
-                        <span className="btn btn-lg btn-success ps-3 pe-4 rounded-pill me-4">Your BMI: <span id="yourBmi">{renderBMI()}</span></span>
+                        <span className="btn btn-lg btn-success ps-3 pe-4 rounded-pill me-4">
+                            Your BMI: {showBMI && (
+                                <span id="yourBmi">{calculateBMI()}</span>
+                            )}
+                        </span>
                     </div>
                 </div>
             </div>
