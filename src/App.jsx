500795a233e571292c1fc5099a13f3dad279d740
Humberto Nacif <gigantedeluz@gmail.com>
2023-06-23 20:36:40 -0300

    creating the table image section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Table from "./components/Table";
 
 function App() {
   return (
     <>
     <Header/>
     <Form/>
+    <Table/>
     </>
   )
 }
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,3 +1,16 @@
+import styles from './Table.module.css'
+import image from '../../assets/bmitable.png'
+
 const Table = () => {
-    
+    return (
+        <div className={styles.container_table}>
+            <div className="row">
+                <span className="title-color fs-4 mt-3">Check the table below:</span>
+                <div className="w-100 mt-1"></div>
+                <img className={styles.img_table} src={image} alt="BMI table" />
+            </div>
+        </div>
+    )
 }
+
+export default Table;
